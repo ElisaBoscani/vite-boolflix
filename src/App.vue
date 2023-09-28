f21d54556ac03d193481a534c48e5a7c110f13c2
Elisa Boscani <elisabosc90@gmail.com>
2023-09-28 15:12:30 +0200

    creato lista dei film cercandoli dall'input

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
-<script></script>
+<script>
+import { store } from "./store";
+export default {
+  name: "App",
+  data() {
+    return {
+      store,
+    };
+  },
+  created() {
+    store.fetchFilm();
+  },
+};
+</script>
 
-<template></template>
+<template>
+  <div>
+    <input type="text" v-model="store.searchText" />
+    <button @click="store.fetchFilm">Cerca</button>
+  </div>
+  <div class="p-3" v-for="items in store.filmData">
+    <ul>
+      <li>Titolo: {{ items.title }}</li>
+      <li>Titolo Originale: {{ items.original_title }}</li>
+      <li>Lingua: {{ items.original_language }}</li>
+      <li>Voto: {{ items.vote_average }}</li>
+    </ul>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -1,6 +1,16 @@
 import { reactive } from "vue";
 import axios from "axios";
 export const store = reactive({
+  searchText: "",
   base_url:
-    "https://api.themoviedb.org/3/search/movie?api_key=0cc3e4c26383ecd435926783b5016c67&query=ritorno+al+fut",
+    "https://api.themoviedb.org/3/search/movie?api_key=0cc3e4c26383ecd435926783b5016c67&query=",
+  filmData: [],
+
+  fetchFilm() {
+    axios.get(this.base_url + `${this.searchText}`).then((response) => {
+      console.log(response);
+      this.filmData = response.data.results;
+      console.log(this.filmData);
+    });
+  },
 });
